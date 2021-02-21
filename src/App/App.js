744507a5fd2b33ec48c8f26a79d5468a861c1bf0
ly son trung <trung.lyson@haravan.com>
2021-02-21 23:46:58 +0700

    update

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,35 @@
 import './App.css';
-import { SideMenu, Header } from '../components'
-import { CssBaseline, makeStyles } from '@material-ui/core'
+import { SideMenu, Header, PageHeader } from '../components'
+import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)'
+      }
+    }
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true
+    }
+  }
+})
 
 
 const useStyles = makeStyles({
@@ -13,14 +42,19 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles()
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader
+          title='Page Header'
+          subTitle='Page description'
+          icon={<PeopleOutlineIcon />}
+        />
       </div>
       <CssBaseline />
 
-    </div>
+    </ThemeProvider>
 
 
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,10 @@
 import SideMenu from './SideMenu'
 import Header from './Header'
+import PageHeader from './PageHeader'
+
 
 export {
     SideMenu,
-    Header
+    Header,
+    PageHeader
 }
